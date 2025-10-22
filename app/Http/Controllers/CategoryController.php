97b9f0607d48cfba039b884dc9579397e62e38e4
Moataz1121 <moatazibrahim04@gmail.com>
2025-10-22 18:58:28 +0300

    add controller and form request for crud operation for the category

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,7 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\Category;
-use Illuminate\Http\Request;
+use App\Http\Requests\StoreCategoryRequest;
+use App\Http\Requests\UpdateCategoryRequest;
 
 class CategoryController extends Controller
 {
@@ -12,9 +13,8 @@ class CategoryController extends Controller
      */
     public function index()
     {
-        //
-        $catregories = Category::paginate(10)->get();
-        return view('categories.index', compact('catregories'));
+        $categories = Category::paginate(10);
+        return view('categories.index', get_defined_vars());
     }
 
     /**
@@ -22,46 +22,51 @@ class CategoryController extends Controller
      */
     public function create()
     {
-        //
+        return view('categories.create', get_defined_vars());
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreCategoryRequest $request)
     {
-        //
+        $data = $request->validated();
+        $record = Category::create($data);
+        return redirect()->route('categories.index')->with('success', 'Category created successfully');
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Category $category)
     {
-        //
+        return view('categories.show', get_defined_vars());
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(Category $category)
     {
-        //
+        return view('categories.edit', get_defined_vars());
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(UpdateCategoryRequest $request, Category $category)
     {
-        //
+        $data = $request->validated();
+        $category->update($data);
+        return redirect()->route('categories.index')->with('success', 'Category updated successfully');
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Category $category)
     {
-        //
+        $category->delete();
+        return redirect()->route('categories.index')->with('success', 'Category deleted successfully');
     }
 }
